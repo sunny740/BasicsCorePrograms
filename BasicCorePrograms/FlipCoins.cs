c3ee9f79f8ed62089d205192961b8b736258455d
sunny740 <sunnysejwarvitm005@gmail.com>
2022-04-11 15:45:43 +0530

    [Sunny] Add: FlipCoinPercentage

---
+++ BasicCorePrograms/FlipCoins.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class FlipCoins
+    {
+        public void FlipCoin()
+        {
+            int tailcount = 0, headcount = 0;
+
+            Random random = new Random();
+
+            Console.WriteLine("Enter nuber of times to flip a coin");
+            int numbofTimes = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= numbofTimes; i++)
+            {
+                double input = random.NextDouble();
+                if (input < 0.5)
+                    tailcount++;
+                else
+                    headcount++;
+            }
+            Console.WriteLine("headcount:{0} tailcount:{1}", headcount, tailcount);
+            double tailPercentage = (tailcount * 100) / numbofTimes;
+            double headPercentage = (headcount * 100) / numbofTimes;
+            Console.WriteLine("Head Percentage:{0}% Tail Percentage:{1}%", headPercentage, tailPercentage);
+
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 
-namespace Snake_and_Ladder
+namespace BasicCorePrograms
 {
-    class PlayerPostionZero
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Basic Core Programms");
-            Console.ReadLine();
+            Console.WriteLine("Welcome to Basic Programs");
+            bool end = true;
+            Console.WriteLine("1. FlipCoin\n2. Leap Year\n3. Power Of Two Table\n4. End the Program");
+            while (end == true)
+            {
+                Console.WriteLine("Take an option to execute");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FlipCoins flipCoin = new FlipCoins();
+                        flipCoin.FlipCoin();
+                        break;
+                    case 4:
+                        end = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter Proper Option To Execute");
+                        break;
+                }
+            }
         }
     }
 }
