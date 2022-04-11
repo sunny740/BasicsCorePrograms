63a6a7287d4f313f29d940c4eadb1a1e938a5362
sunny740 <sunnysejwarvitm005@gmail.com>
2022-04-11 15:17:36 +0530

    [Sunny] Add: Welcome To Basic Core Programs

---
+++ BasicCorePrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Snake_and_Ladder
+{
+    class PlayerPostionZero
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Basic Core Programms");
+            Console.ReadLine();
+        }
+    }
+}
